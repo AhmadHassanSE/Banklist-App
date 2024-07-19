442843cbb50f623c8bd872c99269ebd15e4b2626
Ahmad Hassan <165102183+AhmadHassanSE@users.noreply.github.com>
2024-07-19 22:41:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -225,16 +225,4 @@ btnLoan.addEventListener('click', function (e) {
   inputLoanAmount.blur();
 });
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
 
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
-
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
